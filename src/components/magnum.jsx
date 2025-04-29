47acd1c6a8e6838d4c96dfa31ac360ebb238abd8
Asubek <asubekerakyluly@gmail.com>
2025-04-29 13:04:36 +0500

    Magnum

---
+++ src/components/magnum.jsx
@@ -0,0 +1,273 @@
+import React from 'react'
+import "../index.css"
+
+function magnum() {
+  return (<>
+        <header className='flex items-center justify-between '>
+            <nav className='flex p-4'>
+                <svg fill="#fa0000" width="30px" height="30px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" id="Glyph" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" stroke="#fa0000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M29,16c0,1.104-0.896,2-2,2H11c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C28.104,14,29,14.896,29,16z" id="XMLID_352_"></path><path d="M29,6c0,1.104-0.896,2-2,2H11C9.896,8,9,7.104,9,6s0.896-2,2-2h16C28.104,4,29,4.896,29,6z" id="XMLID_354_"></path><path d="M29,26c0,1.104-0.896,2-2,2H11c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C28.104,24,29,24.896,29,26z" id="XMLID_356_"></path><path d="M3,6c0,1.103,0.897,2,2,2s2-0.897,2-2S6.103,4,5,4S3,4.897,3,6z" id="XMLID_358_"></path><path d="M3,16c0,1.103,0.897,2,2,2s2-0.897,2-2s-0.897-2-2-2S3,14.897,3,16z" id="XMLID_360_"></path><path d="M3,26c0,1.103,0.897,2,2,2s2-0.897,2-2s-0.897-2-2-2S3,24.897,3,26z" id="XMLID_362_"></path></g></svg> 
+                <img width={"220px"} src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Magnum_Cash_%26_Carry.svg/2560px-Magnum_Cash_%26_Carry.svg.png" alt="" />
+             </nav>
+             <div className='flex '>
+                    <h2 className='text-red-500 flex gap-5 w-70 h-4'>Magnum corp. <svg width="15px" height="15px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ff0000" stroke="#ff0000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>send_round [#ff0000]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-60.000000, -959.000000)" fill="#ff0000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M15.732,809.137 L21.547,803.322 C21.859,803.01 22.393,803.222 22.406,803.663 L22.444,805.029 C22.46,805.581 22.92,806 23.472,806 L23.25,806 C23.802,806 24,805.524 24,804.972 L24,801 C24,799.9 23.1,799 22,799 L18.483,799 C17.93,799 17,799.425 17,799.977 L17,799.98 C17,800.532 17.647,800.994 18.199,801.009 L19.733,801.05 C20.174,801.061 20.387,801.595 20.076,801.907 L14.289,807.723 C13.899,808.113 13.913,808.746 14.304,809.137 C14.694,809.527 15.341,809.528 15.732,809.137 M24,812.011 L24,817.015 C24,818.117 23.55,819 22.448,819 L6.44,819 C5.338,819 4,818.117 4,817.015 L4,801.007 C4,799.904 5.338,799 6.44,799 L11.444,799 C11.996,799 12.444,799.448 12.444,800 C12.444,800.553 11.996,801 11.444,801 L7.444,801 C6.892,801 6,801.458 6,802.011 L6,815.015 C6,816.117 7.338,817 8.44,817 L21.444,817 C21.996,817 22,816.563 22,816.011 L22,812.011 C22,811.458 22.447,811.011 23,811.011 C23.552,811.011 24,811.458 24,812.011" id="send_round-[#ff0000]"> </path> </g> </g> </g> </g></svg> </h2>
+                    <div className='flex justify-center item-center w-50 relative border-1'>
+                        <svg className='absolute right-45 top-1' width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#0000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                        <input type="text" placeholder='    Пойск' />
+                    </div>
+                 
+             </div>
+             <div className='flex gap-6'>
+                <select>
+                <option>По скидке</option>
+                </select>
+                <div>
+                    <p className='text-red-500 flex'> <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> Алм. </p>
+                </div> 
+                <div>
+                    <select className='text-red-500'>
+                        <option  >RU</option>
+                    </select>
+                </div> 
+            </div>
+            </header>
+        <aside className='flex gap-20 justify-center'>
+            <p>Коталог скидок</p>
+            <p>Акций</p>
+            <p>Magnum Club</p>
+            <p>Magnum Go</p>
+            <p>Наши Бренды</p>
+            <p>Готовим с Magnum</p>
+            <p>Магазин</p>
+            <p>Контакты</p>
+            <p>О компаний</p>
+        </aside>
+        <main className='grid grid-cols-4'>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+            <article className='w-80 h-120 border-1 ml-5 mt-5'>
+                 <div className='flex justify-between p-2 items-center'>
+                    <h1 className='text-white text-2xl border-1 bg-red-500 p-2 w-18 rounded-2xl'> -25% </h1>
+                    <p>До 30.04</p>
+                </div>
+                <div>
+                    <img className='p-4' width={"300px"} src="https://static.insales-cdn.com/images/products/1/2611/870607411/import_files_4c_4c65ec5ce8b911ed812a0cc47a206601_39b77c7fe8ba11ed812a0cc47a206601.png" alt="" />
+                </div> 
+                <div className='p-5 text-2xl'>
+                    <p>Макароны "Makfa" в ассорти</p>
+                </div> 
+                <div className='flex-col h-20 w-20 bg-yellow-300 ml-3 pl-7 pt-3 ' >
+                    <del>454 ₸</del>
+                   <p>339 ₸</p>
+                </div>
+            </article>
+        </main>
+  </>)
+}
+
+export default magnum
